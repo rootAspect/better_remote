df3011808ea20cb14df0f1f99e9aee1d9bf55177
rootAspect <root_aspect@protonmail.com>
2020-05-02 18:40:31 +0300

    Araba kumandası

---
+++ locales/en.lua
@@ -0,0 +1,11 @@
+Locales['en'] = {
+  ['now_connected'] = 'Anahtarınız şu plakalı araç ile bağlandı ~g~%s',
+  ['unlocked'] = 'Aracınızın kilidi açıldı.',
+  ['locked'] = 'Aracınız kilitlendi.',
+  ['engine_on'] = 'Aracınızın motoru çalıştırıldı.',
+  ['engine_off'] = 'Aracınızın motoru kapatıldı.',
+  ['already_locked'] = 'Araç zaten kilitli.',
+  ['already_unlocked'] = 'Aracın kilidi zaten açıldı.',
+  ['out_of_range'] = 'Aracınız çok uzakta, sinyal çok zayıf.',
+  ['not_connected'] = 'Anahtarınız hiç bir araca bağlanmamış.'
+}
